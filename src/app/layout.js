e2caaab18d19b20aa53b376c39304da0c5719e71
Alexmz1 <menez.alexis19@gmail.com>
2025-12-24 14:55:19 +0100

    Add cookie banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import CookieBanner from '@/components/CookieBanner'
 import SessionProvider from '@/components/SessionProvider'
 
 export const metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
           <Header />
           <main>{children}</main>
           <Footer />
+          <CookieBanner />
         </SessionProvider>
       </body>
     </html>
